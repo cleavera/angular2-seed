641fa3982e544e8664a50e0005d6aa894e199d40
Anthony Cleaver <anthony.cleaver@midlandhr.co.uk>
2016-11-11 11:59:47 +0000

    Made all orchestrators display block

--- src/app/decorators/Orchestrator.decorator.ts
+++ src/app/decorators/Orchestrator.decorator.ts
@@ -3,10 +3,10 @@ import {$snakeCase} from "../../helpers/SnakeCase.helper";
 import {Resolve} from "../service/Resolver.annotation";
 
 export function Orchestrator({name, template, resolve }: {name: string, templateUrl: string, resolve?: any}): ClassDecorator {
-  console.log(`o-${$snakeCase(name)}`);
   let componentDecorator = Component({
     template,
-    selector: `o-${$snakeCase(name)}`
+    selector: `o-${$snakeCase(name)}`,
+    styles: [':host { display: block; }']
   });
 
   return function(Class: any): void {
